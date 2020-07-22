456e7ed6cf3763e28d96ac6ad19026c73a919e2d
Decu98 <decuux@gmail.com>
2020-07-22 17:06:17 +0200

    Dodanie ROUTINGu do GET, POST, PUT, DELETE

--- test.js
+++ test.js
@@ -3,10 +3,22 @@ const app = express()
 const port = 3000
 const path = require('path')
  
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname+'/site.html'))
+app.get('/book/:bookId', function (req, res) {
+  res.send("Pobranie ksiazki")
 })
  
+app.put('/book', function (req, res) {
+  res.send("Dodanie ksiazki")
+})
+
+app.post('/book/:bookId', function (req, res) {
+  res.send("Edycja ksiazki")
+})
+
+app.delete('/book/:bookId', function (req, res) {
+  res.send("Usuniecie ksiazki")
+})
+
 app.listen(port, () => 
     console.log(`Example app listening at http://localhost:${port}`)
 )
